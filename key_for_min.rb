644c12ed37f0578db49855721dd53d8b6dec25e1
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 01:38:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,17 @@
 
 def key_for_min_value(name_hash)
   if name_hash == {}
-  arrayofmaxes = []
-  min_name = ""
-  name_hash.each do |names, values|
-    arrayofmaxes.push(values)
-  end 
-  max = arrayofmaxes.max
-  name_hash.each do |names, value|
-    if value <= max
-      max = value
-      min_name = names
+    arrayofmaxes = []
+    min_name = ""
+    name_hash.each do |names, values|
+      arrayofmaxes.push(values)
     end 
-  end 
+    max = arrayofmaxes.max
+    name_hash.each do |names, value|
+      if value <= max
+        max = value
+        min_name = names
+      end 
+    end 
   min_name
 end
