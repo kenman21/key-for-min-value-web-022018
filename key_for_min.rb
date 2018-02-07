fd97067350a461c7aea4033ec3678a4b6b0575d8
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 01:13:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   max = 0
+  max_name = ""
   name_hash.each do |names, value|
     if value > max
       max = value
+      max_name = names
     end 
-  
+  end 
+  max_name
 end
